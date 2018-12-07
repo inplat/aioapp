366453658de4a4ddfdc757275f8718583f9b4045
Konstantin Stepanov <konstantin.stepanov@inplat.ru>
2018-12-07 14:21:45 +0300

    easy import

--- aioapp/__init__.py
+++ aioapp/__init__.py
@@ -6,5 +6,8 @@ __author__ = """Konstantin Stepanov"""
 __version__ = '0.0.2b5'
 
 from . import app, error
+from .app import Component, Application
+from .tracer import Span
+from .config import Config
 
-__all__ = ['app', 'error']
+__all__ = ['app', 'error', 'Component', 'Application', 'Span', 'Config']
